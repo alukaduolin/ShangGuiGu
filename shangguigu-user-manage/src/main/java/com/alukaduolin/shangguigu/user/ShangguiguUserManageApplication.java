3e9bd1bea9c093d3293ae4cab5288d8934477dfe
alukaduolin <763064101@qq.com>
2020-02-01 18:51:56 +0800

    加入用户管理模块

---
+++ shangguigu-user-manage/src/main/java/com/alukaduolin/shangguigu/user/ShangguiguUserManageApplication.java
@@ -0,0 +1,13 @@
+package com.alukaduolin.shangguigu.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShangguiguUserManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShangguiguUserManageApplication.class, args);
+    }
+
+}
