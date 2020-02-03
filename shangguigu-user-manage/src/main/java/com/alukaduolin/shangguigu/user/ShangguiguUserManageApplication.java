012acd21e6d0d93b51cbd504ac82e560093664a0
alukaduolin <763064101@qq.com>
2020-02-03 17:23:40 +0800

    第二次提交 2020.2.3 到尚硅谷课程第二节结束,从建立项目开始到delUser

--- shangguigu-user-manage/src/main/java/com/alukaduolin/shangguigu/user/ShangguiguUserManageApplication.java
+++ shangguigu-user-manage/src/main/java/com/alukaduolin/shangguigu/user/ShangguiguUserManageApplication.java
@@ -2,12 +2,14 @@ package com.alukaduolin.shangguigu.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@ComponentScan
+@MapperScan(basePackages = "com.alukaduolin.shangguigu.user.mapper")
 public class ShangguiguUserManageApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(ShangguiguUserManageApplication.class, args);
+        SpringApplication.run(ShangguiguUserManageApplication.class,args);//这里可能出问题,视频太模糊
     }
-
 }
